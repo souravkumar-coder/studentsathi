1d10c68a76896747e35135a8e2f89896d45ab3b9
sourav-observer <hiddenvillain01@gmail.com>
2026-08-22 18:32:58 +0530

    Fix: Add metro config, ignore cache

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,12 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, {
-  input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
-});
+// Fix cache issue
+config.watchFolders = [__dirname];
+config.resolver.blockList = [
+  /.*\/\.cache\/.*/,
+  /.*\/node_modules\/.*\/\.cache\/.*/,
+];
+
+module.exports = config;
