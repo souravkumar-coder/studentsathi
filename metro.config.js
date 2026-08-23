efd4c5a55c9c48c05cd51d67c8bcea59653d5f05
sourav-observer <hiddenvillain01@gmail.com>
2026-08-23 11:25:40 +0530

    Fix: Simplify metro config

--- metro.config.js
+++ metro.config.js
@@ -2,27 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Fix cache issue
-config.watchFolders = [__dirname];
-config.resolver.blockList = [
-  /.*\/\.cache\/.*/,
-  /.*\/node_modules\/.*\/\.cache\/.*/,
-];
-
-module.exports = config;
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// ✅ Add this for web scrolling fix
-config.transformer = {
-  ...config.transformer,
-  unstable_allowRequireContext: true,
-};
-
-config.resolver = {
-  ...config.resolver,
-  sourceExts: [...config.resolver.sourceExts, 'css'],
-};
-
 module.exports = config;
